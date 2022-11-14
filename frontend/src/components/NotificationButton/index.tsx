7d05a515a7336070425e47926b03ed697e909d24
Lucas Ferreira <ferreiralucas090@gmail.com>
2022-11-13 23:17:18 -0300

    feat: send sms notification

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,12 +1,25 @@
+import axios from "axios";
 import icon from "../../assets/img/notification-icon.svg"
+import { BASE_URL } from "../../utils/request";
 import "./styles.css"
+
+type Props = {
+    saleId: number;
+}
+
+function handleClick(id: number){
+    axios(`${BASE_URL}/sales/${id}/notification`)
+        .then(response => {
+            console.log("SUCESSO")
+        })
+}
     
-function NotificationButton() {
+function NotificationButton( {saleId} : Props ) {
     return (
-        <div className="dsmeta-red-btn">
-            <img src={icon} alt="" />
+        <div className="dsmeta-red-btn" onClick={() => handleClick(saleId)}>
+            <img src={icon} alt="Notificate" />
         </div>
     )
 }
 
-export default NotificationButton
+export default NotificationButton;
